ec8445f538ad48de77777a7bfa2b93c9ae98bd97
Tycho Hartingsveld <99062718@mydavinci.nl>
2021-09-07 09:18:39 +0200

    update

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Tycho Hartingsveld
+# 99062718
+
+print("Hello wereld van...\n")
+print("    pppp    y     y   ttttttt   h   h    oooo   nnnn")
+print("    p   p    y   y       t      h   h   o    o  n   n")
+print("    p   p     y y        t      h   h   o    o  n   n")
+print("    pppp       y         t      hhhhh   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h   o    o  n   n")
+print("    p          y         t      h   h    oooo   n   n")
